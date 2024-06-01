7baa3c6d18c84ef76ac4a143f9e55a858e3cbbd2
Hakob Hakobyan <hak.hakobian@gmail.com>
2024-06-01 12:33:20 +0400

    Fix preview margin in editors

--- src/components/settings/SettingsContext.tsx
+++ src/components/settings/SettingsContext.tsx
@@ -227,7 +227,7 @@ const SettingsProvider: React.FC<React.PropsWithChildren> = ({children}) => {
 
   const renderChildren = (): ReactNode => {
     if (showControls) {
-      return <div className={'reacg-galler-wrapper'}>{children}</div>;
+      return <div className={'reacg-gallery-wrapper'}>{children}</div>;
     }
     return children;
   };
