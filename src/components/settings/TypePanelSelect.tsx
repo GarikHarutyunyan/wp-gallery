91df65b29bf3b4728c500e9b78e5f085159a2435
Garik <garik.harutyunian@gmail.com>
2024-06-22 22:33:50 +0400

    fullwidth

--- src/components/settings/TypePanelSelect.tsx
+++ src/components/settings/TypePanelSelect.tsx
@@ -1,4 +1,4 @@
-import {MenuItem, Select, Typography} from '@mui/material';
+import {Box, MenuItem, Select, Typography} from '@mui/material';
 import {ISelectOption} from 'components/controls';
 import {useAppInfo} from 'contexts/AppInfoContext';
 import {GalleryType, GalleryTypeOptions} from 'data-structures';
@@ -19,33 +19,35 @@ const TypePanelSelect: React.FC<ITypePanelSelectProps> = ({
   const {pluginUrl} = useAppInfo();
 
   return (
-    <Select
-      onChange={(event) => onChange(event.target.value as GalleryType)}
-      value={activeValue}
-      sx={{width: '200px'}}
-    >
-      {GalleryTypeOptions.map(({value, title}: ISelectOption) => {
-        const isSelected: boolean = value === activeValue;
+    <Box sx={{margin: '6px'}}>
+      <Select
+        onChange={(event) => onChange(event.target.value as GalleryType)}
+        value={activeValue}
+        sx={{width: '100%'}}
+      >
+        {GalleryTypeOptions.map(({value, title}: ISelectOption) => {
+          const isSelected: boolean = value === activeValue;
 
-        return (
-          <MenuItem value={value}>
-            <div className={'type-panel-select__body'}>
-              <img
-                style={{
-                  backgroundColor: isSelected ? activeColor : inactiveColor,
-                }}
-                src={`${pluginUrl}/assets/images/${value}.png`}
-                className={'type-panel-select__image'}
-                alt={title}
-              />
-              <Typography className={'type-panel-select__title'}>
-                {title}
-              </Typography>
-            </div>
-          </MenuItem>
-        );
-      })}
-    </Select>
+          return (
+            <MenuItem value={value}>
+              <div className={'type-panel-select__body'}>
+                <img
+                  style={{
+                    backgroundColor: isSelected ? activeColor : inactiveColor,
+                  }}
+                  src={`${pluginUrl}/assets/images/${value}.png`}
+                  className={'type-panel-select__image'}
+                  alt={title}
+                />
+                <Typography className={'type-panel-select__title'}>
+                  {title}
+                </Typography>
+              </div>
+            </MenuItem>
+          );
+        })}
+      </Select>
+    </Box>
   );
 };
 
