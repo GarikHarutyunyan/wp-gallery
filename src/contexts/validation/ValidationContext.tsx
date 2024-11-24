3c706b817d23cab84a8d5872813f3a9f33552a7e
Garik <garik.harutyunian@gmail.com>
2024-11-24 21:23:36 +0400

    use validation endpoint to check if user is PRO

--- src/contexts/validation/ValidationContext.tsx
+++ src/contexts/validation/ValidationContext.tsx
@@ -9,13 +9,13 @@ const ValidationProvider: React.FC<React.PropsWithChildren> = ({children}) => {
   const [isProUser, setIsProUser] = useState(false);
 
   const validate = async () => {
-    const fetchUrl: string | undefined = undefined;
-    // 'https://regallery.team/core/wp-json/reacgcore/v1/templates'; //baseUrl      ? baseUrl + 'templates'      : undefined;
+    const fetchUrl: string | undefined =
+      'https://regallery.team/core/wp-json/reacgcore/v1/paidUser/'; //baseUrl      ? baseUrl + 'templates'      : undefined;
 
     if (fetchUrl) {
       try {
         const response = await axios.get(fetchUrl);
-        const newIsProUser: boolean = response.data;
+        const newIsProUser: boolean = response.data.paid_user;
 
         setIsProUser(newIsProUser);
       } catch (error) {
