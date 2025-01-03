2e8f34643d0c99ce3efe2525d114a53526fa1a7d
Garik <garik.harutyunian@gmail.com>
2025-01-03 22:57:30 +0400

    minor changes

--- src/contexts/templates/PremiumOffer.tsx
+++ src/contexts/templates/PremiumOffer.tsx
@@ -9,7 +9,6 @@ const contentTextStyle = {
   letterSpacing: '1.5px',
 };
 
-// fontFamily: 'Lilita One',
 const PremiumOffer = () => {
   return (
     <Box className={'premium-offer'}>
@@ -25,14 +24,14 @@ const PremiumOffer = () => {
         component={'div'}
         sx={{
           color: 'black',
-          fontWeight: '700',
+          fontWeight: 600,
           display: 'flex',
           gap: 1,
           alignItems: 'center',
           justifyContent: 'center',
           textAlign: 'center',
           padding: '30px',
-          fontFamily: 'Montserrat',
+          fontFamily: 'lexend',
         }}
       >
         {'This pre-built template is part of our Pro plan.'}
@@ -49,6 +48,7 @@ const PremiumOffer = () => {
             padding: '6px 40px',
             fontSize: 'large',
             boxShadow: 'unset',
+            fontWeight: 400,
           }}
         >
           {'GET STARTED'}
