88c2b2e9cf47cdfa7b6583df69b135d63b8dbbb7
Garik <garik.harutyunian@gmail.com>
2025-02-06 02:17:57 +0400

    push webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -201,39 +201,43 @@ module.exports = function (webpackEnv) {
     // These are the "entry points" to our application.
     // This means they will be the "root" imports that are included in JS bundle.
     entry: paths.appIndexJs,
-    // output: {
-    //   // The build folder.
-    //   path: paths.appBuild,
-    //   // Add /* filename */ comments to generated require()s in the output.
-    //   pathinfo: isEnvDevelopment,
-    //   // There will be one main bundle, and one file per asynchronous chunk.
-    //   // In development, it does not produce real files.
-    //   filename: isEnvProduction
-    //     ? 'static/js/[name].[contenthash:8].js'
-    //     : isEnvDevelopment && 'static/js/bundle.js',
-    //   // There are also additional JS chunk files if you use code splitting.
-    //   chunkFilename: isEnvProduction
-    //     ? 'static/js/[name].[contenthash:8].chunk.js'
-    //     : isEnvDevelopment && 'static/js/[name].chunk.js',
-    //   assetModuleFilename: 'static/media/[name].[hash][ext]',
-    //   // webpack uses `publicPath` to determine where the app is being served from.
-    //   // It requires a trailing slash, or the file assets will get an incorrect path.
-    //   // We inferred the "public path" (such as / or /my-project) from homepage.
-    //   publicPath: paths.publicUrlOrPath,
-    //   // Point sourcemap entries to original disk location (format as URL on Windows)
-    //   devtoolModuleFilenameTemplate: isEnvProduction
-    //     ? info =>
-    //         path
-    //           .relative(paths.appSrc, info.absoluteResourcePath)
-    //           .replace(/\\/g, '/')
-    //     : isEnvDevelopment &&
-    //       (info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')),
-    // },
     output: {
-      path: 'C:/xampp/htdocs/wordpress/wp-content/plugins/regallery/assets/js', //path.resolve(__dirname, 'build'),
+      // The build folder.
+      // path: paths.appBuild,
+      path: 'C:/wamp64/www/wordpress/wp-content/plugins/regallery/assets/js', //path.resolve(__dirname, 'build'),
+      // Add /* filename */ comments to generated require()s in the output.
+      pathinfo: isEnvDevelopment,
+      // There will be one main bundle, and one file per asynchronous chunk.
+      // In development, it does not produce real files.
+      // filename: isEnvProduction
+      //   ? 'static/js/[name].[contenthash:8].js'
+      //   : isEnvDevelopment && 'static/js/bundle.js',
       filename: 'wp-gallery.js', // Change this to your desired filename
-      publicPath: '/wordpress/wp-content/plugins/gallery/assets/js/',
+      // There are also additional JS chunk files if you use code splitting.
+      chunkFilename: isEnvProduction
+        ? 'wp-gallery/wp-gallery.[name].[contenthash:8].chunk.js'
+        : isEnvDevelopment && 'wp-gallery/wp-gallery.[name].chunk.js',
+      // assetModuleFilename: 'static/media/[name].[hash][ext]',
+      // webpack uses `publicPath` to determine where the app is being served from.
+      // It requires a trailing slash, or the file assets will get an incorrect path.
+      // We inferred the "public path" (such as / or /my-project) from homepage.
+      // publicPath: paths.publicUrlOrPath,
+      publicPath: '/wordpress/wp-content/plugins/regallery/assets/js/',
+      // Point sourcemap entries to original disk location (format as URL on Windows)
+      // devtoolModuleFilenameTemplate: isEnvProduction
+      //   ? (info) =>
+      //       path
+      //         .relative(paths.appSrc, info.absoluteResourcePath)
+      //         .replace(/\\/g, '/')
+      //   : isEnvDevelopment &&
+      //     ((info) =>
+      //       path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')),
     },
+    // output: {
+    //   path: 'C:/wamp64/www/wordpress/wp-content/plugins/regallery/assets/js', //path.resolve(__dirname, 'build'),
+    //   filename: 'wp-gallery.js', // Change this to your desired filename
+    //   publicPath: '/wordpress/wp-content/plugins/regallery/assets/js/',
+    // },
     cache: {
       type: 'filesystem',
       version: createEnvironmentHash(env.raw),
