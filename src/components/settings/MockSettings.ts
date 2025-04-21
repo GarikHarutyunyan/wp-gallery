512fa448bbf405b54e1e8f74463bbbf4ee1d7578
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2025-04-21 23:56:48 +0400

    isFullCoverImage instead of IsFullCoverImage

--- src/components/settings/MockSettings.ts
+++ src/components/settings/MockSettings.ts
@@ -108,7 +108,7 @@ const slideshowMockSettings: ISlideshowSettings = {
   thumbnailPadding: 0,
   thumbnailGap: 10,
   isSlideshowAllowed: true,
-  IsFullCoverImage: false,
+  isFullCoverImage: false,
 };
 
 const generalMockSettings: IGeneralSettings = {
@@ -150,7 +150,7 @@ const lightboxMockSettings: ILightboxSettings = {
   captionFontFamily: 'Roboto',
   captionColor: 'White',
   imageAnimation: LightboxImageAnimation.BLUR,
-  IsFullCoverImage: false,
+  isFullCoverImage: false,
 };
 
 const cubeMockSettings: ICubeSettings = {
